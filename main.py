ec08827a3fe573b905f2c1d919f1e0c7d90b5ca5
Ian Chartier <80764650+Ian091@users.noreply.github.com>
2023-02-10 16:24:27 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from easygopigo3 import EasyGoPiGo3
 import time
 
 gp3_obj = EasyGoPiGo3()
-ultrasonic_sensor = gp3_obj.init_ultrasonic_sensor('AD2')
+ultrasonic_sensor_1 = gp3_obj.init_ultrasonic_sensor()
+ultrasonic_sensor_2 = gp3_obj.init_ultrasonic_sensor('AD2')
 
 for i in range(20):
-    distance_cm = ultrasonic_sensor.read()
-    distance_inches = ultrasonic_sensor.read_inches()
+    distance_cm_sensor_1 = ultrasonic_sensor.read()
+    distance_inches_sensor_1 = ultrasonic_sensor.read_inches()
+    print(f"Distance in inches: {distance_inches}")
+    print(f"Distance in cm: {distance_cm}")
+    print('---------------------------------')
+    distance_cm_sensor_2 = ultrasonic_sensor.read()
+    distance_inches_sensor_2 = ultrasonic_sensor.read_inches()
     print(f"Distance in inches: {distance_inches}")
     print(f"Distance in cm: {distance_cm}")
     time.sleep(1)
-
